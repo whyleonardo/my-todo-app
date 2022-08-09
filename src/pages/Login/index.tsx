08d32fa5c6df315006d5e043e12cc3efaad69db1
Christian <pleasefloyd73@gmail.com>
2022-08-09 11:16:28 -0300

    feat: login page and register page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,31 @@
-import { Flex, Box, Spacer, Input, Image, Button } from "@chakra-ui/react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Flex, Box, Text, Spacer, Input, Image, Button } from "@chakra-ui/react"
+
+
+interface LoginProps {
+  username: string
+  password: string
+}
 
 export const Login = () => {
+
+  const [userInfo, setUserInfo] = useState<LoginProps[]>([])
+
+  const handleChangeLoginInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(userInfo)
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <Flex h='100vh'>
       <Flex w='50%' bg='gray' align='center' justify='center'>
         <Box>
           <Image
-            boxSize='550px'
+            boxSize='500px'
             src='./ImageLogo.svg'
           />
         </Box>
@@ -14,15 +33,71 @@ export const Login = () => {
 
       <Spacer />
 
-      <Flex w='50%' align='center' justify='center' bg='black'>
+      <Flex w='50%' direction='column' align='center' gap='2rem' justify='center' bg='black'>
+
+        <Box>
+          <Text
+            as='h1'
+            color='white'
+            fontWeight='black'
+            fontSize='3xl'
+            textAlign='center'
+            lineHeight='2rem'
+            mb='.3rem'
+          >
+            Manage Your <Text as='span' color='tomato'>Tasks</Text>
+          </Text>
+
+          <Text
+            as='h1'
+            color='white'
+            fontWeight='black'
+            fontSize='3xl'
+            textAlign='center'
+            lineHeight='2rem'
+          >
+            Manage Your <Text as='span' color='tomato'>Day</Text>
+          </Text>
+        </Box>
+
         <Flex direction='column' gap='0.5rem'>
-          <Input type='text' placeholder='Username' />
-          <Input type='password' placeholder='Password' />
+          <Input
+            name="username"
+            type='text'
+            focusBorderColor='tomato'
+            placeholder='Username'
+            onChange={handleChangeLoginInfo}
+          />
+          <Input
+            name="password"
+            type='password'
+            focusBorderColor='tomato'
+            placeholder='Password'
+            onChange={handleChangeLoginInfo}
+          />
 
           <Button fontWeight='black'> Login </Button>
         </Flex>
+
+        <Text as='span'>
+          Don't have account? {''}
+          <Text
+            as='span'
+            textDecoration='underline'
+            cursor='pointer'
+            color='purple'
+            filter='auto'
+            _hover={{ brightness: '100%' }}
+          >
+            <Link to='/register'>
+              Register here.
+            </Link>
+          </Text>
+        </Text>
       </Flex>
-    </Flex>
+
+
+    </Flex >
   )
 }
 
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,89 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Flex, Box, Text, Spacer, Input, Image, Button } from "@chakra-ui/react"
+interface RegisterProps {
+  username: string
+  password: string
+}
+
 export const Register = () => {
+
+  const [userInfo, setUserInfo] = useState<RegisterProps[]>([])
+
+  const handleChangeRegisterInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(userInfo)
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value
+    })
+  }
   return (
-    <div>Register</div>
+    <Flex h='100vh'>
+      <Flex w='50%' bg='gray' align='center' justify='center'>
+        <Box>
+          <Image
+            boxSize='500px'
+            src='./ImageLogo.svg'
+          />
+        </Box>
+      </Flex>
+
+      <Spacer />
+
+      <Flex w='50%' direction='column' align='center' gap='1.5rem' justify='center' bg='black'>
+
+        <Box>
+          <Text
+            as='h1'
+            color='white'
+            fontWeight='black'
+            fontSize='3xl'
+            textAlign='center'
+            lineHeight='2rem'
+          >
+            Create Your User
+          </Text>
+        </Box>
+
+        <Flex direction='column' gap='0.5rem'>
+          <Input
+            name="username"
+            type='text'
+            focusBorderColor='tomato'
+            placeholder='Username'
+            onChange={handleChangeRegisterInfo}
+          />
+          <Input
+            name="password"
+            type='password'
+            focusBorderColor='tomato'
+            placeholder='Password'
+            onChange={handleChangeRegisterInfo}
+
+          />
+
+          <Button fontWeight='black'> Register </Button>
+        </Flex>
+
+        <Text as='span'>
+          Already have an account? {''}
+          <Text
+            as='span'
+            textDecoration='underline'
+            cursor='pointer'
+            color='purple'
+            filter='auto'
+            _hover={{ brightness: '100%' }}
+          >
+            <Link to='/'>
+              Login here.
+            </Link>
+          </Text>
+        </Text>
+      </Flex>
+
+
+    </Flex >
   )
 }
 
