731398667ded8a39cf76c025c89ea5894abfdfd8
Christian <pleasefloyd73@gmail.com>
2022-08-09 11:24:46 -0300

    feat: env variables

--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,1 +1,12 @@
 import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: 'todo-leoapp.firebaseapp.com',
+  projectId: 'todo-leoapp',
+  storageBucket: 'todo-leoapp.appspot.com',
+  messagingSenderId: '635312358214',
+  appId: import.meta.env.VITE_APP_ID
+}
+
+const app = initializeApp(firebaseConfig)
